f894ae856c5a255ac6c7e47540efed4f32e3590e
Halfdan <halfdansc@gmail.com>
2025-03-11 09:21:41 +0100

    fixed a small bug with the countdown timer

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-import { countDownStart } from "./timer.js";
-
 // pokemon frames
 const pokemonFrame = document.getElementsByClassName('pokemon-frames')[0];
 
@@ -8,37 +6,43 @@ const gameTimer = document.getElementById('game-timer');
 const ranButton = document.getElementsByClassName('random-button')[0];
 const score = document.getElementById('score');
 
-// countdown and interval used for the timer
-let countDown = 5;
+// countdown and interval used for the timer plus the players score
+let countDown = 5.00;
 let playerScore = 0;
+let interval;
 
 const gameLoop = () => {
     ranButton.style.display = 'none';
-    let interval = setInterval(() => {
-        countDown--;
+    displayTimer();
+    score.innerHTML = 'score: 0';
+
+    interval = setInterval(() => {
+        countDown-= 0.01;
         displayTimer();
 
         if (countDown <= 0) {
             gameOver();
         }
-    }, 1000);
 
+    }, 10);
 };
 
 const gameOver = () => {
     score.innerHTML = 'GAME OVER'
+    ranButton.style.display = 'inline-block';
+    countDown = 5.00;
+    clearInterval(interval);
 };
-
-// eventhandlers
-ranButton.addEventListener('click', () => {
-    //countDownStart(countDown, interval, gameTimer);
-    gameLoop();
-});
 
 const displayTimer = () => {
     if (countDown > 0) {
-        gameTimer.innerHTML = `<strong>Time left: ${countDown}</strong>`;
+        gameTimer.innerHTML = `<strong>Time left: ${countDown.toFixed(2)}</strong>`;
     } else {
         gameTimer.innerHTML = `<strong>Time's up!</strong>`;
     }
 };
+
+// eventhandlers
+ranButton.addEventListener('click', () => {
+    gameLoop();
+});
